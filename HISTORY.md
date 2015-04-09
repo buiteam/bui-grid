19e817ec749443ea516f807900caacfdfbf05c19
jared <jaredlee.ljj@alipay.com>
2015-04-09 16:41:42 +0800

    [fix] bug of append summary description repeatedly

--- HISTORY.md
+++ HISTORY.md
@@ -5,3 +5,7 @@
 ## 1.1.0
 
 `new` It is the first version of bui-grid.
+
+## 1.1.5
+
+- [fix] 修复统计插件中表头重复填充的问题
--- src/plugins/summary.js
+++ src/plugins/summary.js
@@ -266,7 +266,7 @@ BUI.augment(summary,{
       }
       firstCell.html(getInnerTemplate(text));
     }else{
-      firstCell.html(getInnerTemplate(title + ':'));
+      firstCell.html(getInnerTemplate(title + ': '));
     }
   },
   //获取汇总模板
